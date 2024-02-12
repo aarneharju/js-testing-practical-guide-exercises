1d4e0cee2ab52c9e5cd35136cfa0476d389b14ca
Aarne Harju <aarne.harju@sogeti.com>
2024-02-12 15:26:57 +0200

    Refactor: Forces the input to numeric format

--- basic-testing-frontend/src/math.js
+++ basic-testing-frontend/src/math.js
@@ -2,7 +2,7 @@ export function add(numbers) {
   let sum = 0;
 
   for (const number of numbers) {
-    sum += number;
+    sum += +number; // Plus (+) in front of a variable returns the numeric representation of the object = forces it to have a numeric value if possible
   }
   return sum;
 }
