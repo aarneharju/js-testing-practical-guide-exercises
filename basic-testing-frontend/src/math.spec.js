b3e24e6c22611173bd5f357c23153925816f6372
Aarne Harju <aarne.harju@sogeti.com>
2024-02-12 15:27:54 +0200

    Added two more tests to check input format

--- basic-testing-frontend/src/math.spec.js
+++ basic-testing-frontend/src/math.spec.js
@@ -13,3 +13,19 @@ it("should add together all number values in an array", () => {
     const expectedResult = numbers.reduce((total, currentNumber) => currentNumber + total, 0);
     expect(result).toBe(expectedResult);
 })
+
+it("should yield NaN if at least one of the numbers is invalid", () => {
+    const numbers = ["invalid", 1];
+    
+    const result = add(numbers);
+
+    expect(result).toBeNaN();
+})
+
+it("should yield a correct sum if an array of numeric string values is provided", () => {
+    const numbers = ["1", "2"];
+
+    const result = add(numbers);
+
+    const expectedResult = numbers.reduce((total, currentNumber) => currentNumber + total, 0);
+})
