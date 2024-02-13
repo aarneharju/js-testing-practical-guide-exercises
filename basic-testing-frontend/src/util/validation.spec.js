1dd45b785491aa1535af26a96029a2bd904699bf
Aarne Harju <aarne.harju@sogeti.com>
2024-02-13 13:34:58 +0200

    Refactor: Added suites (describe) to make it more clear which functions the tests work with

--- basic-testing-frontend/src/util/validation.spec.js
+++ basic-testing-frontend/src/util/validation.spec.js
@@ -1,62 +1,68 @@
-import { it, expect } from "Vitest";
+import { it, expect, describe } from "Vitest";
 import { validateStringNotEmpty, validateNumber } from "./validation";
 
-it("should throw an error if input is an empty string", () => {
-    const input = "";
+describe("validateStringNotEmpty()", () => {
+    describe("You can also nest describe blocks inside each other like this", () => {
+        it("should throw an error if input is an empty string", () => {
+            const input = "";
+            
+            const testFunction = () => {
+                validateStringNotEmpty(input);
+            }
+        
+            expect(testFunction).toThrow(/Invalid input - must not be empty./);
+        })
+    })
     
-    const testFunction = () => {
-        validateStringNotEmpty(input);
-    }
-
-    expect(testFunction).toThrow(/Invalid input - must not be empty./);
-})
-
-it("should not throw an error if input string is not empty", () => {
-    const input = "not an empty string"
+    it("should not throw an error if input string is not empty", () => {
+        const input = "not an empty string"
+        
+        const testFunction = () => {
+            validateStringNotEmpty(input);
+        }
     
-    const testFunction = () => {
-        validateStringNotEmpty(input);
-    }
-
-    expect(testFunction).not.toThrow(/Invalid input - must not be empty./);
-})
-
-it("should throw an error if input is just spaces because they get trimmed out", () => {
-    const input = "          ";
+        expect(testFunction).not.toThrow(/Invalid input - must not be empty./);
+    })
     
-    const testFunction = () => {
-        validateStringNotEmpty(input);
-    }
-
-    expect(testFunction).toThrow(/Invalid input - must not be empty./);
-})
-
-it("should throw an error if input is not a number", () => {
-    const input = {}; // note: empty array [] is undefined in javascript and will not pass if used here
+    it("should throw an error if input is just spaces because they get trimmed out", () => {
+        const input = "          ";
+        
+        const testFunction = () => {
+            validateStringNotEmpty(input);
+        }
     
-    const testFunction = () => {
-        validateNumber(input);
-    }
-
-    expect(testFunction).toThrow(/Invalid number input./);
+        expect(testFunction).toThrow(/Invalid input - must not be empty./);
+    })    
 })
 
-it("should not throw an error if input is a number", () => {
-    const input = 4;
-
-    const testFunction = () => {
-        validateNumber(input);
-    }
-
-    expect(testFunction).not.toThrow(/Invalid number input./);
-})
-
-it("should throw an error if input is a string", () => {
-    const input = "2"
-
-    const testFunction = () => {
-        validateNumber(input);
-    }
-
-    expect(testFunction).toThrow(/Invalid number input./)
+describe("validateNumber()", () => {
+    it("should throw an error if input is not a number", () => {
+        const input = {}; // note: empty array [] is undefined in javascript and will not pass if used here
+        
+        const testFunction = () => {
+            validateNumber(input);
+        }
+    
+        expect(testFunction).toThrow(/Invalid number input./);
+    })
+    
+    it("should not throw an error if input is a number", () => {
+        const input = 4;
+    
+        const testFunction = () => {
+            validateNumber(input);
+        }
+    
+        expect(testFunction).not.toThrow(/Invalid number input./);
+    })
+    
+    it("should throw an error if input is a string", () => {
+        const input = "2"
+    
+        const testFunction = () => {
+            validateNumber(input);
+        }
+    
+        expect(testFunction).toThrow(/Invalid number input./)
+    })    
 })
