bf764711269903c53fce62103f25a966d41a6167
Aarne Harju <aarne.harju@sogeti.com>
2024-02-16 10:24:12 +0200

    Refactor: Created suite 'transformToNumber()' and moved the tests under it

--- basic-testing-frontend/src/util/numbers.spec.js
+++ basic-testing-frontend/src/util/numbers.spec.js
@@ -1,43 +1,46 @@
-import { it, expect } from "Vitest";
+import { it, expect, describe } from "Vitest";
 import { transformToNumber } from "./numbers";
 
-it("should return a number if a string that can be converted to a number was given as input", () => {
-    const input = "2";
-
-    const result = transformToNumber(input);
-
-    expect(result).toBeTypeOf("number");
-    expect(result).toBe(2);
-})
-
-it("should return NaN if a string that can't be converted to a number was given as input", () => {
-    const input = "invalid input string";
-
-    const result = transformToNumber(input);
+describe("transformToNumber()", () => {
+    it("should return a number if a string that can be converted to a number was given as input", () => {
+        const input = "2";
     
-    expect(result).toBeNaN();
-})
-
-it("should return NaN if input is empty (undefined)", () => {
-    // const input = undefined; <- this could be used as input too
+        const result = transformToNumber(input);
     
-    const result = transformToNumber();
+        expect(result).toBeTypeOf("number");
+        expect(result).toBe(2);
+    })
     
-    expect(result).toBeNaN;
-})
-
-it("should return Infinity if string 'Infinity' was given as input", () => {
-    const input = "Infinity";
-
-    const result = transformToNumber(input);
+    it("should return NaN if a string that can't be converted to a number was given as input", () => {
+        const input = "invalid input string";
     
-    expect(result).toBe(Infinity);
-})
-
-it("should return NaN if an array was given as input", () => {
-    const input = [1, 3];
-
-    const result = transformToNumber(input);
-
-    expect(result).toBeNaN();
+        const result = transformToNumber(input);
+        
+        expect(result).toBeNaN();
+    })
+    
+    it("should return NaN if input is empty (undefined)", () => {
+        // const input = undefined; <- this could be used as input too
+        
+        const result = transformToNumber();
+        
+        expect(result).toBeNaN;
+    })
+    
+    it("should return Infinitxy if string 'Infinity' was given as input", () => {
+        const input = "Infinity";
+    
+        const result = transformToNumber(input);
+        
+        expect(result).toBe(Infinity);
+    })
+    
+    it("should return NaN if an array was given as input", () => {
+        const input = [1, 3];
+    
+        const result = transformToNumber(input);
+    
+        expect(result).toBeNaN();
+    })
+    
 })
