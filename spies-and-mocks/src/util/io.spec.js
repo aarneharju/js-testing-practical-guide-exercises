5246dd0b3f5fdc7d6c46e9d9c6c08736ee5a8f7a
Aarne Harju <aarne.harju@sogeti.com>
2024-02-20 11:45:13 +0200

    Mocked the path function to test arguments the writeData() function was called with

--- spies-and-mocks/src/util/io.spec.js
+++ spies-and-mocks/src/util/io.spec.js
@@ -3,12 +3,22 @@ import writeData from "./io";
 import { promises as fs } from "fs";
 
 vi.mock("fs");
+vi.mock("path", () => {
+    return {
+        default: {
+            join: (...args) => {
+                return args[args.length - 1];
+            }
+        }
+    }
+});
 
 it("should execute the writeFile method", () => {
     const testData = "Test";
     const testFilename = "test.txt";
     writeData(testData, testFilename);
 
-    expect(fs.writeFile).toBeCalled();
+    expect(fs.writeFile).toBeCalledWith(testFilename, testData);
+    // expect(fs.writeFile).toBeCalled();
     // return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
 })
