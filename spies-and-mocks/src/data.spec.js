96450c078d57c14e007fcd5ff00a348c88a632f1
Aarne Harju <aarne.harju@sogeti.com>
2024-02-20 12:00:29 +0200

    You can use mockImplementationOnce() to temporarily change how a mock function works

--- spies-and-mocks/src/data.spec.js
+++ spies-and-mocks/src/data.spec.js
@@ -4,6 +4,7 @@ import { generateReportData } from "./data";
 describe("generateReportData()", () => {
     it("should execute logging function if it is provided", () => {
         const logger = vi.fn();
+        // logger.mockImplementationOnce(() => {});
         generateReportData(logger);
         expect(logger).toBeCalled();
     })
