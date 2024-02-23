aa6ce4967d4f989d431112bf78c75adf0222db08
Aarne Harju <aarne.harju@sogeti.com>
2024-02-23 15:08:17 +0200

    Added a test to dom.js to check that the error message div is initially empty

--- advanced-mocking/util/dom.spec.js
+++ advanced-mocking/util/dom.spec.js
@@ -1,18 +1,22 @@
-import { it, expect, describe, vi } from "vitest";
+import { it, expect, describe, vi, beforeEach } from "vitest";
 import fs from "fs";
 import path from "path";
 import { Window } from "happy-dom";
 import { showError } from "./dom";
 
 const htmlDocPath = path.join(process.cwd(), "index.html");
-const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
+let htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
 
 const window = new Window();
 const document = window.document;
-document.write(htmlDocumentContent);
 vi.stubGlobal("document", document);
 
 describe("showError()", () => {
+    beforeEach(() => {
+        document.body.innerHTML = ""; // document.write appends so the document content needs to be reset to empty first
+        document.write(htmlDocumentContent);
+    })
+
     it("should add an error paragraph to the id='errors' element", () => {
         const testMessage = "Error message";
 
@@ -21,5 +25,12 @@ describe("showError()", () => {
         const errorMessageElement = document.querySelector("#errors > p");
 
         expect(errorMessageElement.innerHTML).toBe(testMessage);
+    })
+
+    it("should not contain an error paragraph initially", () => {
+        const errorContainerElement = document.querySelector("#errors");
+        console.log(errorContainerElement.outerHTML);
+
+        expect(errorContainerElement.childElementCount).toBe(0);
     })
 })
