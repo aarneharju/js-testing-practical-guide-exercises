bbde087f25116bf5d8dce7bdffa9ee67955f11c0
Aarne Harju <aarne.harju@sogeti.com>
2024-02-23 14:41:02 +0200

    Added tests (2) to posts.js

--- advanced-mocking/posts/posts.spec.js
+++ advanced-mocking/posts/posts.spec.js
@@ -17,11 +17,25 @@ describe("extractPostData()", () => {
     })
 
     it("should extact the title and content from the provided form data", () => {
-        
-
         const data = extractPostData(testFormData);
 
         expect(data.title).toBe(testTitle);
         expect(data.content).toBe(testContent);
+    })
+
+    it("should throw an error if title is not provided", () => {
+        testFormData.title = "";
+        
+        const data = () => extractPostData(testFormData);
+
+        expect(data).toThrow("A title must be provided.");
+    })
+
+    it("should throw an error if content is not provided", () => {
+        testFormData.content = "";
+        
+        const data = () => extractPostData(testFormData);
+
+        expect(data).toThrow("Content must not be empty!");
     })
 })
